3ee335fb1c31d1940fe4261d4d31a24bf93291b9
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2025-06-15 15:48:03 +0200

    feat: auth ui

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -143,7 +143,7 @@ export const SignUpView = () => {
                   </Alert>
                 )}
                 <Button type="submit" disabled={pending} className="w-full">
-                  Sign in
+                  Sign up
                 </Button>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                   <span className="bg-card text-muted-foreground relative z-10 px-2">Or continue with</span>
